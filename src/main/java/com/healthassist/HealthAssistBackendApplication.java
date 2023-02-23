8d6ca50020e5748de93a1bbfac024837634e52fe
Smit Pateliya <smitpa2000@gmail.com>
2023-02-23 09:47:36 -0500

    AB#27 test commit

--- src/main/java/com/healthassist/HealthAssistBackendApplication.java
+++ src/main/java/com/healthassist/HealthAssistBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HealthAssistBackendApplication {
 
 	public static void main(String[] args) {
+		//Testing
 		SpringApplication.run(HealthAssistBackendApplication.class, args);
 	}
 
