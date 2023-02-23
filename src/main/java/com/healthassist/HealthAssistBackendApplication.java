0d39411c3832f7c6b113739e5cd7025baa7a0131
Ashwin Raghunath <ashwinr2296@gmail.com>
2023-02-22 22:35:51 -0500

    Project setup

---
+++ src/main/java/com/healthassist/HealthAssistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.healthassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAssistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAssistBackendApplication.class, args);
+	}
+
+}
