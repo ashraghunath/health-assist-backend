e08894eee0f785a69762b793ea2f6ab220ab3a39
Ashwin Raghunath <ashwinr2296@gmail.com>
2023-03-03 18:01:04 -0500

    AB#26 boolean_questions document name changed

--- src/main/java/com/healthassist/entity/Question.java
+++ src/main/java/com/healthassist/entity/Question.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("questions")
+@Document("boolean_questions")
 @Setter
 @Getter
 public class Question {
