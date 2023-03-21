225650966f11250d22b89240e2ecc9f39d79caec
Ashwin Raghunath <ashwinr2296@gmail.com>
2023-03-21 11:06:18 -0400

    Adding DB env variable

--- src/main/java/com/healthassist/config/CORSConfig.java
+++ src/main/java/com/healthassist/config/CORSConfig.java
@@ -15,15 +15,23 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSConfig {
+
+	/**
+	 * Add this to your application.properties file
+	 * *************************************
+	 * site.origin=http://localhost:3000
+	 * *************************************
+	 */
+	@Value("${site.origin}")
+    private String site;
+
     @Bean
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        // @Todo Don't do this in production, use a proper list  of allowed origins
-        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        config.setAllowedOriginPatterns(Collections.singletonList(site));
         config.setAllowedHeaders(Collections.singletonList("*"));
-//        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
